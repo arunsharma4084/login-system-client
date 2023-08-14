e308b30afa0e7967e1cb67326542af3faecfb949
arunsharma4084 <97881862+arunsharma4084@users.noreply.github.com>
2023-08-14 20:35:53 +0530

    New base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,3 +1,3 @@
 import axios from "axios"
 
-export const clientAPI = axios.create({baseURL:"http://localhost:3000"})
+export const clientAPI = axios.create({baseURL:"https://login-system-7vgk.onrender.com/"})
